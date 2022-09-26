abb28f425a1835cab1f3411a032761e119a7e974
Will <werobinson6@gmail.com>
2022-09-26 17:53:08 +0200

    add layout generation for trypsin script

--- examples/trypsin_oscillator.py
+++ examples/trypsin_oscillator.py
@@ -1,7 +1,8 @@
 from DataBinder.Constructors import Topology as topol
 from DataBinder.Compilers import to_function
+from DataBinder.Visualisation import Layouts
 
-topology_file = "../data/trypsin_oscillator.txt"
+topology_file = "data/trypsin_oscillator.txt"
 
 # load topology
 topology = topol.from_text(topology_file)
@@ -9,3 +10,9 @@ topology = topol.from_text(topology_file)
 function_text = to_function(topology)
 
 print(function_text)
+
+# Generate a layout
+pos = Layouts.generate_topology_layout(topology, algorithm = "sfdp")
+
+for p in pos:
+    print(f"{p}: x: {pos[p][0]}, y: {pos[p][1]}")
