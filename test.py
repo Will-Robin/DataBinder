018d0f7683ffdb5ad2667d1de520f5393b78e651
Will <werobinson6@gmail.com>
2022-09-26 12:57:25 +0200

    update test script with example outputs

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 from DataBinder.Constructors import DataContainer as cont
 from DataBinder.Constructors import Topology as topol
-from DataBinder.Compilers import equation_system
 from DataBinder.Compilers import to_function
 from DataBinder.Compilers import write_adjacency_matrix
 from DataBinder.Binders import data_topology
@@ -16,21 +15,26 @@ topology_container = topol.from_text("example_data/exampleReactionList.txt")
 # Validate that the data and topology are compatible
 messages = data_topology.validate(data_container, topology_container)
 if len(messages) > 0:
-    print("# Found issues:")
-    for m in messages:
-        print(f"# {m}")
+    print(f"# Found {len(messages)} issue(s):")
+    for c,m in enumerate(messages, 1):
+        print(f"# {c}. {m}")
+print()
 
 # Put information from the data into the topology
 bound = data_topology.bind_data_topology(data_container, topology_container)
 
-# Generate a layout
-pos = Layouts.generate_topology_layout(topology_container, algorithm="fdp")
-
 # Create python code for the topology
 function_text = to_function(topology_container)
+
+print("Example function compilation:\n")
+print(function_text)
 
 # Create an adjacency matrix
 adj_list = write_adjacency_matrix(topology_container)
 
+print("Example adjacency list compilation:\n")
 for a in adj_list:
     print(a)
+
+# Generate a layout
+pos = Layouts.generate_topology_layout(topology_container, algorithm="fdp")
