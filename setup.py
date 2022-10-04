29d429f415ec575269da0d2e7df1ffddea7d1db6
W_E_Robinson <wills_email>
2022-10-04 18:05:51 +0200

    add setup for pip install

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, version
+
+setup(
+    name="DataBinder",
+    version="0.0.0",
+    author="William E. Robinson",
+    packages=["DataBinder"],
+    install_requires=[],
+)
